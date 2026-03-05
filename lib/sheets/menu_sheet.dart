4a12c339f3b2eefa6fbee9450060abfcbe3b909b
plotsklapps <plotsklapps@gmail.com>
2026-03-05 15:37:50 +0100

    - Updated to TEST2, to check.

--- lib/sheets/menu_sheet.dart
+++ lib/sheets/menu_sheet.dart
@@ -59,7 +59,7 @@ class MenuSheet extends StatelessWidget {
           ListTile(
             onTap: updateApp,
             leading: const FaIcon(FontAwesomeIcons.github),
-            title: const Text('Current Version: 0.0.1+3 TEST1'),
+            title: const Text('Current Version: 0.0.1+3 TEST2'),
             subtitle: const Text('Deployed 20260305'),
             trailing: const FaIcon(FontAwesomeIcons.arrowsRotate),
           ),
