9cd8548ef91a5686bc6d2e5fbc198a909e39f7d9
plotsklapps <plotsklapps@gmail.com>
2026-07-21 16:05:52 +0200

    - Added fontawesomeicons in favor of lucideicons. Working on icons.dart file to replace all icons (WIP).

---
+++ lib/theme/icons.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class IconUtils {
+  static const Icon chevronLeft = FaIcon(FontAwesomeIcons.angleLeft);
+  static const Icon chevronUp = FaIcon(FontAwesomeIcons.angleUp);
+  static const Icon chevronRight = FaIcon(FontAwesomeIcons.angleRight);
+  static const Icon chevronDown = FaIcon(FontAwesomeIcons.angleDown);
+  static const Icon chevronsLeft = FaIcon(FontAwesomeIcons.anglesLeft);
+  static const Icon chevronsUp = FaIcon(FontAwesomeIcons.anglesUp);
+  static const Icon chevronsRight = FaIcon(FontAwesomeIcons.anglesRight);
+  static const Icon chevronsDown = FaIcon(FontAwesomeIcons.anglesDown);
+  static const Icon circleLeft = FaIcon(FontAwesomeIcons.circleChevronLeft);
+  static const Icon circleUp = FaIcon(FontAwesomeIcons.circleChevronUp);
+  static const Icon circleRight = FaIcon(FontAwesomeIcons.circleChevronRight);
+  static const Icon circleDown = FaIcon(FontAwesomeIcons.circleChevronDown);
+
+  static const Icon play = FaIcon(FontAwesomeIcons.circlePlay);
+  static const Icon pause = FaIcon(FontAwesomeIcons.circlePause);
+  static const Icon stop = FaIcon(FontAwesomeIcons.circleStop);
+  static const Icon reset = FaIcon(FontAwesomeIcons.circleXmark);
+  static const Icon add = FaIcon(FontAwesomeIcons.circlePlus);
+  static const Icon remove = FaIcon(FontAwesomeIcons.circleMinus);
+
+  static const Icon check = FaIcon(FontAwesomeIcons.circleCheck);
+  static const Icon unCheck = FaIcon(FontAwesomeIcons.circle);
+
+  static const Icon calendar = FaIcon(FontAwesomeIcons.calendar);
+  static const Icon history = FaIcon(FontAwesomeIcons.clockRotateLeft);
+  static const Icon statistics = FaIcon(FontAwesomeIcons.chartColumn);
+  static const Icon calculator = FaIcon(FontAwesomeIcons.calculator);
+
+  static const Icon incognito = FaIcon(FontAwesomeIcons.userSecret);
+  static const Icon publicKey = FaIcon(FontAwesomeIcons.key);
+  static const Icon secretKey = FaIcon(FontAwesomeIcons.lock);
+  static const Icon copy = FaIcon(FontAwesomeIcons.clone);
+  static const Icon save = FaIcon(FontAwesomeIcons.floppyDisk);
+
+  static const Icon login = FaIcon(FontAwesomeIcons.arrowRightToBracket);
+  static const Icon logout = FaIcon(FontAwesomeIcons.arrowRightFromBracket);
+
+  static const Icon body = FaIcon(FontAwesomeIcons.childReaching);
+  static const Icon weight = FaIcon(FontAwesomeIcons.weightScale);
+  static const Icon bmi = FaIcon(FontAwesomeIcons.scaleBalanced);
+  static const Icon bodyFat = FaIcon(FontAwesomeIcons.fire);
+  static const Icon male = FaIcon(FontAwesomeIcons.mars);
+  static const Icon female = FaIcon(FontAwesomeIcons.venus);
+
+  static const Icon palette = FaIcon(FontAwesomeIcons.palette);
+  static const Icon wakelockOn = FaIcon(FontAwesomeIcons.mobileScreen);
+  static const Icon wakelockOff = FaIcon(FontAwesomeIcons.mobile);
+  static const Icon lightMode = FaIcon(FontAwesomeIcons.lightbulb);
+  static const Icon darkMode = FaIcon(FontAwesomeIcons.solidLightbulb);
+  static const Icon useKGS = FaIcon(FontAwesomeIcons.scaleBalanced);
+  static const Icon useLBS = FaIcon(FontAwesomeIcons.weightHanging);
+}
