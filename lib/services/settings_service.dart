1ca36b7d27b1ac50ad7030ff2dd94932ba0df344
plotsklapps <plotsklapps@gmail.com>
2026-08-24 12:33:40 +0200

    refactor: replace Flutter material imports with material_ui and remove cupertino_icons dependency

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:gymply/models/bodymetrics_model.dart';
 import 'package:gymply/models/settings_model.dart';
 import 'package:gymply/models/strength_model.dart';
--- lib/theme/flexscheme.dart
+++ lib/theme/flexscheme.dart
@@ -1,6 +1,5 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:signals/signals_flutter.dart';
 
 const Color kOrange = Color(0xFFFCB075);
